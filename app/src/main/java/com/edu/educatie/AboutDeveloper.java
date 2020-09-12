5a3d77325e4ae862d5971f68c15353eb5976ad09
DevanandSB <devanandsbofficial@gmail.com>
2020-09-12 13:12:50 +0530

    Added Developer Information

---
+++ app/src/main/java/com/edu/educatie/AboutDeveloper.java
@@ -0,0 +1,14 @@
+package com.edu.educatie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutDeveloper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_developer);
+    }
+}
