1044975621b3ef9b00a8e00d87828a6d77d5892d
DevanandSB <devanandsbofficial@gmail.com>
2020-11-01 21:43:52 +0530

    Adding Notes Feature

---
+++ app/src/main/java/com/edu/educatie/dao/NoteDao.java
@@ -0,0 +1,24 @@
+package com.edu.educatie.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.edu.educatie.entities.Note;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+
+    @Query("SELECT * FROM notes ORDER BY id DESC")
+    List<Note> getAllNotes();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertNote(Note note);
+
+    @Delete
+    void deleteNote(Note note);
+}
---
+++ app/src/main/java/com/edu/educatie/database/NotesDatabase.java
@@ -0,0 +1,25 @@
+package com.edu.educatie.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.edu.educatie.dao.NoteDao;
+import com.edu.educatie.entities.Note;
+
+@Database(entities = Note.class, version = 1, exportSchema = false)
+public abstract class NotesDatabase extends RoomDatabase {
+
+    private static NotesDatabase notesDatabase;
+
+    public static synchronized NotesDatabase getDatabase(Context context) {
+        if(notesDatabase == null) {
+            notesDatabase = Room.databaseBuilder(context, NotesDatabase.class , "notes_db").build();
+        }
+        return notesDatabase;
+    }
+
+    public abstract NoteDao noteDao();
+}
