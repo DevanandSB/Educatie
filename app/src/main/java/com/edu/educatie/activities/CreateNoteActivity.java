a6b308f04fcdd5cc035ec8ed77ecdd0c3787fb15
DevanandSB <devanandsbofficial@gmail.com>
2020-11-02 19:25:38 +0530

    Adding Notes Feature

--- app/src/main/java/com/edu/educatie/activities/CreateNoteActivity.java
+++ app/src/main/java/com/edu/educatie/activities/CreateNoteActivity.java
@@ -9,10 +9,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.edu.educatie.R;
 import com.edu.educatie.database.NotesDatabase;
 import com.edu.educatie.entities.Note;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
 import java.text.SimpleDateFormat;
@@ -46,8 +48,9 @@ public class CreateNoteActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 saveNote();
-            }
-        });
+            }});
+
+        initMiscellaneous();
     }
     private void saveNote() {
         if(inputNoteTitle.getText().toString().trim().isEmpty()) {
@@ -83,5 +86,22 @@ public class CreateNoteActivity extends AppCompatActivity {
         }
 
         new SaveNoteTask().execute();
+    }
+
+    private void initMiscellaneous() {
+        final LinearLayout layoutMiscellaneous = findViewById(R.id.layoutMiscellaneous);
+        final BottomSheetBehavior<LinearLayout> bottomSheetBehavior = BottomSheetBehavior.from(layoutMiscellaneous);
+        layoutMiscellaneous.findViewById(R.id.textMiscellaneous).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(bottomSheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                } else {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                }
+            }
+        });
+
+
     }
 }
