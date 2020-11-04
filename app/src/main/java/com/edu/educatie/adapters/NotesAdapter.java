392346870b198b689d32f72dadd2a3c11d627478
DevanandSB <devanandsbofficial@gmail.com>
2020-11-04 22:42:08 +0530

    Adding Notes Feature

--- app/src/main/java/com/edu/educatie/adapters/NotesAdapter.java
+++ app/src/main/java/com/edu/educatie/adapters/NotesAdapter.java
@@ -3,6 +3,8 @@ package com.edu.educatie.adapters;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,16 +19,22 @@ import com.edu.educatie.R;
 import com.edu.educatie.entities.Note;
 import com.edu.educatie.listerners.NotesListener;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NoteViewHolder>{
 
     private List<Note> notes;
     private NotesListener notesListener;
+    private Timer timer;
+    private List<Note> notesSource;
 
     public NotesAdapter(List<Note> notes, NotesListener notesListener) {
         this.notes = notes;
         this.notesListener = notesListener;
+        notesSource = notes;
     }
 
     public NotesAdapter(List<Note> notes) {
@@ -100,6 +108,40 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NoteViewHold
             }else {
                 imageNote.setVisibility(View.GONE);
             }
+        }
+    }
+
+    public void searchNotes(final String searchKeyword) {
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if(searchKeyword.trim().isEmpty()) {
+                    notes = notesSource;
+                } else {
+                    ArrayList<Note> temp = new ArrayList<>();
+                    for(Note note : notesSource) {
+                        if(note.getTitle().toLowerCase().contains(searchKeyword.toLowerCase()) ||
+                        note.getSubtitle().toLowerCase().contains(searchKeyword.toLowerCase()) ||
+                        note.getNoteText().toLowerCase().contains(searchKeyword.toLowerCase())) {
+                            temp.add(note);
+                        }
+                    }
+                    notes = temp;
+                }
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        notifyDataSetChanged();
+                    }
+                });
+            }
+        },500);
+    }
+
+    public void cancelTimer() {
+        if (timer != null) {
+            timer.cancel();
         }
     }
 }
