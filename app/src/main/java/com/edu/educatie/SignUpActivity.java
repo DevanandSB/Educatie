3b838a9ee703491fc0d9022db1f22b306b145034
achuthan02 <achuthan02@gmail.com>
2021-01-06 21:27:22 +0530

    Password Regex Pattern updated

--- app/src/main/java/com/edu/educatie/SignUpActivity.java
+++ app/src/main/java/com/edu/educatie/SignUpActivity.java
@@ -2,15 +2,22 @@ package com.edu.educatie;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SignUpActivity extends AppCompatActivity {
 
     TextView textLogin;
     Button signUpButton;
-
+    EditText textName, textEmail, textPassword;
+    String passwordchecker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,25 @@ public class SignUpActivity extends AppCompatActivity {
 
         textLogin = findViewById(R.id.textSignIn);
         signUpButton = findViewById(R.id.buttonSignUp);
+        textName = findViewById(R.id.inputFirstName);
+        textEmail = findViewById(R.id.inputEmail);
+        textPassword = findViewById(R.id.inputsignupPassword);
+
+        passwordchecker = textPassword.getText().toString();
+
+        if(textName.getText().toString().trim().isEmpty()){
+            FancyToast.makeText(getBaseContext(), "Enter Your Name", FancyToast.LENGTH_SHORT, FancyToast.ERROR, false).show();
+        } else if(textEmail.getText().toString().trim().isEmpty()) {
+            FancyToast.makeText(getBaseContext(), "Enter Your Email", FancyToast.LENGTH_SHORT, FancyToast.ERROR, false).show();
+        } else if(textPassword.getText().toString().trim().isEmpty()) {
+            FancyToast.makeText(getBaseContext(), "Password can't be Empty", FancyToast.LENGTH_SHORT, FancyToast.ERROR, false).show();
+        } else if(!Patterns.EMAIL_ADDRESS.matcher(textEmail.getText().toString()).matches()) {
+            FancyToast.makeText(getBaseContext(), "Enter Valid Email", FancyToast.LENGTH_SHORT, FancyToast.ERROR, false).show();
+        } else if(!validPassword(textPassword.getText().toString())) {
+            FancyToast.makeText(getBaseContext(), "Enter Valid Password", FancyToast.LENGTH_SHORT, FancyToast.ERROR, false).show();
+        } else {
+            signUpButton.setOnClickListener(v -> FancyToast.makeText(getBaseContext(), "Validation Successful", FancyToast.LENGTH_SHORT, FancyToast.SUCCESS, false).show());
+        }
 
         textLogin.setOnClickListener(v -> {
             Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
@@ -26,7 +52,19 @@ public class SignUpActivity extends AppCompatActivity {
             finish();
         });
 
-        signUpButton.setOnClickListener(v -> FancyToast.makeText(getBaseContext(), "Sign up Successful", FancyToast.LENGTH_SHORT, FancyToast.SUCCESS, false).show());
 
+    }
+
+    public boolean checkRegex(String pattern, String input) {
+        Pattern r = Pattern.compile(pattern);
+        return r.matcher(input).matches();
+    }
+
+    public boolean validPassword(String password) {
+        return checkRegex("(?-i)(?=^.{8,}$)((?!.*\\s)(?=.*[A-Z])(?=.*[a-z]))(?=(1)(?=.*\\d)|.*[^A-Za-z0-9])^.*$",passwordchecker);
+//      At least 8 characters long
+//      At least 1 uppercase and 1 lowercase
+//      At least 1 digit (OR) at least 1 alphanumeric.
+//      No spaces.
     }
 }
