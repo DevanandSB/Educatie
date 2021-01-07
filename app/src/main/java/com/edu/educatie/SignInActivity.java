2fd2177a284e5de1c2784ba16a8f8323c44a30dc
achuthan02 <achuthan02@gmail.com>
2021-01-07 20:47:32 +0530

    Validation Completed

--- app/src/main/java/com/edu/educatie/SignInActivity.java
+++ app/src/main/java/com/edu/educatie/SignInActivity.java
@@ -2,6 +2,8 @@ package com.edu.educatie;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -23,6 +25,10 @@ public class SignInActivity extends AppCompatActivity {
 
         textSignUp = findViewById(R.id.textSignUp);
         test = findViewById(R.id.tempLogin);
+        textEmail = findViewById(R.id.inputEmail);
+        textPassword = findViewById(R.id.inputPassword);
+
+
         signInButton = findViewById(R.id.buttonSignIn);
 
         textSignUp.setOnClickListener(v -> {
@@ -31,7 +37,20 @@ public class SignInActivity extends AppCompatActivity {
             finish();
         });
 
-        signInButton.setOnClickListener(v -> FancyToast.makeText(getBaseContext(), "Login Successful", FancyToast.LENGTH_SHORT, FancyToast.SUCCESS, false).show());
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(textEmail.getText().toString().trim().isEmpty()) {
+                    FancyToast.makeText(getBaseContext(), "Enter Your Email", FancyToast.LENGTH_SHORT, FancyToast.ERROR, false).show();
+                } else if(!Patterns.EMAIL_ADDRESS.matcher(textEmail.getText().toString()).matches()) {
+                    FancyToast.makeText(getBaseContext(), "Enter Valid Email", FancyToast.LENGTH_SHORT, FancyToast.ERROR, false).show();
+                } else if(textPassword.getText().toString().trim().isEmpty()) {
+                    FancyToast.makeText(getBaseContext(), "Password can't be Empty", FancyToast.LENGTH_SHORT, FancyToast.ERROR, false).show();
+                } else {
+                    FancyToast.makeText(getBaseContext(), "Validation Successful", FancyToast.LENGTH_SHORT, FancyToast.SUCCESS, false).show();
+                }
+            }
+        });
 
         test.setOnClickListener(v -> {
             Intent intent = new Intent(SignInActivity.this, Home.class);
