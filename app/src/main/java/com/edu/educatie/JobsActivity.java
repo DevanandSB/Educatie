2a98cdd460957b01a8e2083e2ea3030c415ff2bb
DevanandSB <devanandsbofficial@gmail.com>
2021-04-05 13:43:58 +0530

    Material UI Implemented

--- app/src/main/java/com/edu/educatie/JobsActivity.java
+++ app/src/main/java/com/edu/educatie/JobsActivity.java
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class JobsActivity extends AppCompatActivity {
 
     RadioButton tcs, wipro, infosys, cts, zoho, techmahindra, others;
-    LinearLayout tcsLayout, wiproLayout, ctsLayout, zoholayout, techMahiLayout, othersLayout;
+    LinearLayout tcsLayout, wiproLayout, infosysLayout, ctsLayout, zoholayout, techMahiLayout, othersLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +24,15 @@ public class JobsActivity extends AppCompatActivity {
         zoho = findViewById(R.id.Zoho);
         techmahindra = findViewById(R.id.techMahindra);
         others = findViewById(R.id.others);
+
+        tcsLayout = findViewById(R.id.tcsLinearLayout);
+        wiproLayout = findViewById(R.id.wiproLinearLayout);
+        infosysLayout = findViewById(R.id.infoysLinearLayout);
+        ctsLayout = findViewById(R.id.ctsLinearLayout);
+        zoholayout = findViewById(R.id.zohoLinearLayout);
+        techMahiLayout = findViewById(R.id.tech_mahindraLinearLayout);
+        othersLayout = findViewById(R.id.OthersLinearLayout);
+
 
 
 
