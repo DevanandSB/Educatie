52c83d26b42a975c270bfc6242b73dbb303779c2
DevanandSB <devanandsbofficial@gmail.com>
2021-04-05 13:40:11 +0530

    Material UI Implemented

--- app/src/main/java/com/edu/educatie/JobsActivity.java
+++ app/src/main/java/com/edu/educatie/JobsActivity.java
@@ -1,15 +1,29 @@
 package com.edu.educatie;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class JobsActivity extends AppCompatActivity {
+
+    RadioButton tcs, wipro, infosys, cts, zoho, techmahindra, others;
+    LinearLayout tcsLayout, wiproLayout, ctsLayout, zoholayout, techMahiLayout, othersLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jobs);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        tcs = findViewById(R.id.TCS);
+        tcs = findViewById(R.id.Wipro);
+        tcs = findViewById(R.id.Infosys);
+        tcs = findViewById(R.id.cts);
+        tcs = findViewById(R.id.Zoho);
+        tcs = findViewById(R.id.techMahindra);
+        tcs = findViewById(R.id.others);
 
     }
 }
